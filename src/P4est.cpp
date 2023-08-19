911116e8aaf68ff9d942f36b5362363c7c9761f8
Damyn Chipman <DamynChipman@u.boisestate.edu>
2023-08-19 14:23:28 -0600

    Improving documentation, cleaning up.

--- src/P4est.cpp
+++ src/P4est.cpp
@@ -4,16 +4,6 @@ namespace EllipticForest {
 
 namespace p4est {
 
-p4est_t* p4est_new_uniform(MPI_Comm mpicomm, std::size_t levels, std::size_t data_size, p4est_init_t init_fn, void* user_pointer) {
-
-    // // Create new p4est connectivity
-    // p4est_connectivity_t* p4est_connectivity_new_unitsquare();
-
-    // // Create new p4est
-    // p4est_t* p4est_new()
-
-}
-
 p4est_connectivity_t* p4est_connectivity_new_square_domain(double x_lower, double x_upper, double y_lower, double y_upper) {
   
     const p4est_topidx_t num_vertices = 4;
